64fd93f9edae9f3d142364456d93eaeb94405422
Daniel Martinez <d.martinez74@gmail.com>
2023-03-06 16:20:13 +0000

    Done until excercise 89

--- exercises/088-Select/app.js
+++ exercises/088-Select/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function select (arr, obj) {
+    let newObj = {}
+    arr.forEach(element => {
+        if (obj[element]) {
+            newObj[element] = obj[element];
+        }        
+    });
+    return newObj
+}
--- exercises/089-getElementsThatEqual10AtProperty/app.js
+++ exercises/089-getElementsThatEqual10AtProperty/app.js
@@ -1,1 +1,29 @@
 // Write your function here
+function getElementsThatEqual10AtProperty(obj, key) {
+    let newArr = [];
+    
+    if (Array.isArray(obj[key])) {
+        let arr = obj[key];
+        for (let i = 0 ; i < arr.length; i++) {
+            if (arr[i] === 10) {
+                newArr.push(arr[i])
+            } 
+        }
+        return newArr;
+        // arr.forEach(element => {
+        //     if (element === 10) {
+        //         newArr.push(element);
+        //     }
+        // }); 
+    }
+    else {
+        return newArr;
+    }
+
+}
+
+let obj = {
+    key: [1000, 10, 50, 10]
+};
+let output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output);
