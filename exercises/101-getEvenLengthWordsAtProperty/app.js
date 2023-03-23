0faac8a3984c391e58a397ff4edc4135b4fd9701
Daniel Martinez <d.martinez74@gmail.com>
2023-03-23 14:24:09 +0000

    Done until exercise 101 (dalmatians)

--- exercises/101-getEvenLengthWordsAtProperty/app.js
+++ exercises/101-getEvenLengthWordsAtProperty/app.js
@@ -1,8 +1,20 @@
 let obj = {
-  key: ['a', 'long', 'game']
+    key: ['a', 'long', 'game']
 };
+
 function getEvenLengthWordsAtProperty(obj, key) {
     // your code here
+    if (obj[key].length == 0 || !Array.isArray(obj[key]) || !obj[key]) {
+        return [];
+    }
+
+    let newArr = [];
+    for (let i = 0; i < obj[key].length; i++) {
+        if (obj[key][i].length % 2 == 0) {
+            newArr.push(obj[key][i])
+        }
+    }
+    return newArr;
 }
 
 let output = getEvenLengthWordsAtProperty(obj, 'key');
