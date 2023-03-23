ef99766c23cf3beeac36f5aa1093191fb4879dd4
Daniel Martinez <d.martinez74@gmail.com>
2023-03-23 14:10:16 +0000

    Done until exercise 100 :)

--- exercises/098-getOddLengthWordsAtProperty/app.js
+++ exercises/098-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,20 @@
 // Write your function here
+function getOddLengthWordsAtProperty(obj, key) {
+    let oddWords = [];
+    if (obj[key].length == 0 || !Array.isArray(obj[key]) || !obj[key]) {
+        return oddWords;
+    }
+
+    for (let i = 0; i < obj[key].length; i++) {
+        if (obj[key][i].length % 2 !== 0) {
+            oddWords.push(obj[key][i])
+        }
+    }
+    return oddWords
+}
+
+let obj = {
+    key: ['It', 'has', 'some', 'words']
+};
+let output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output);
--- exercises/100-getAverageOfElementsAtProperty/app.js
+++ exercises/100-getAverageOfElementsAtProperty/app.js
@@ -1,4 +1,19 @@
 function getAverageOfElementsAtProperty(obj, key) {
-  // your code here
+    // your code here
+    if (obj[key].length === 0 || !Array.isArray(obj[key]) || !obj[key]) {
+        return 0;
+    }
 
+    let total = 0;
+    for (let i = 0; i < obj[key].length; i++) {
+        total += obj[key][i]
+    }
+    let average = total / obj[key].length;
+    return average;
 }
+
+let obj = {
+    key: [1, 2, 3]
+};
+let output = getAverageOfElementsAtProperty(obj, 'key');
+console.log(output);
